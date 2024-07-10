dff190f5a7f5d5dff8451cdc019f68b55ea649ad
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-10 16:18:51 +0700

    no whitelist middleware

--- middleware/IpWhitelist.js
+++ middleware/IpWhitelist.js
@@ -2,24 +2,24 @@ import IpWhitelist from '../models/config/IpWhitelistModel.js';
 
 const ipWhitelistMiddleware = async (req, res, next) => {
     try {
-        // const clientIP = req.ip;
-        const clientServerIp = req.connection.remoteAddress;
-        // const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+        // // const clientIP = req.ip;
+        // const clientServerIp = req.connection.remoteAddress;
+        // // const clientIP = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
 
-        // Cari IP yang sesuai dengan clientIP di database
-        const whitelistEntry = await IpWhitelist.findOne({
-            where: {
-                ip: clientServerIp
-            }
-        });
+        // // Cari IP yang sesuai dengan clientIP di database
+        // const whitelistEntry = await IpWhitelist.findOne({
+        //     where: {
+        //         ip: clientServerIp
+        //     }
+        // });
 
-        // Jika tidak ada whitelist entry yang sesuai, beri response Forbidden
-        if (!whitelistEntry) {
-            return res.status(403).json({
-                status: 0,
-                message: 'Forbidden - Your IP is not allowed to access this resource',
-            });
-        }
+        // // Jika tidak ada whitelist entry yang sesuai, beri response Forbidden
+        // if (!whitelistEntry) {
+        //     return res.status(403).json({
+        //         status: 0,
+        //         message: 'Forbidden - Your IP is not allowed to access this resource',
+        //     });
+        // }
 
         // Lanjutkan jika alamat IP diizinkan
         next();
