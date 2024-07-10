e86b116a32ef96befdec05ea68c5cfd6f3e2b7b5
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-10 15:43:02 +0700

    enc config call

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,8 @@
 import express from "express";
 import cors from "cors";
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 //middleware
 import ipWhitelistMiddleware from '../middleware/IpWhitelist.js';
