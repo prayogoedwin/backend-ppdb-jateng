519a5e69efb69b7ba6180e47a2b34237999b63e0
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-10 14:34:58 +0700

    no redis for windows dev

--- redis.js
+++ redis.js
@@ -1,22 +1,4 @@
-import { createClient } from 'redis';
 import dotenv from 'dotenv';
-
-// Load environment variables
-dotenv.config();
-
-const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379'
-});
-
-redisClient.on('error', (err) => {
-  console.error('Redis connection error:', err);
-});
-
-redisClient.on('connect', () => {
-  console.log('Connected to Redis');
-});
-
-redisClient.connect();
 
 /**
  * Get value from Redis
@@ -24,13 +6,7 @@ redisClient.connect();
  * @returns {Promise<string|null>} - The value associated with the key, or null if the key does not exist
  */
 const redisGet = async (key) => {
-  try {
-    const value = await redisClient.get(key);
-    return value;
-  } catch (error) {
-    console.error('Error getting value from Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 /**
@@ -41,62 +17,28 @@ const redisGet = async (key) => {
  * @returns {Promise<void>}
  */
 const redisSet = async (key, value, expiration = 3600) => {
-  try {
-    await redisClient.set(key, value, { EX: expiration });
-  } catch (error) {
-    console.error('Error setting value in Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 
 // redis.js
 const redisClearKey = async (key) => {
-  try {
-    const result = await redisClient.del(key);
-    console.log(`Deleted key: ${key}, result: ${result}`);
-    return result;
-  } catch (error) {
-    console.error('Error deleting key from Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 // Function to clear all keys from Redis
 const redisClearAll = async () => {
-  try {
-    await redisClient.flushAll();
-  } catch (error) {
-    console.error('Error clearing all keys from Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 
 // Function to get all keys from Redis
 const redisGetAllKeys = async () => {
-  try {
-    const keys = await redisClient.keys('*');
-    return keys;
-  } catch (error) {
-    console.error('Error getting all keys from Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 const redisGetAllKeysAndValues = async () => {
-  try {
-    const keys = await redisClient.keys('*');
-    const keyValues = {};
-    for (const key of keys) {
-      const value = await redisClient.get(key);
-      keyValues[key] = value;
-    }
-    return keyValues;
-  } catch (error) {
-    console.error('Error getting all keys and values from Redis:', error);
-    throw error;
-  }
+  return true;
 };
 
 export { redisGet, redisSet, redisClearKey, redisClearAll, redisGetAllKeys, redisGetAllKeysAndValues };
