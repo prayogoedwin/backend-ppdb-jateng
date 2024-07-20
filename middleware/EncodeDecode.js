9556f419c6fe4f5c52a96ea174d6015af962f365
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-21 01:00:21 +0700

    encode decode

---
+++ middleware/EncodeDecode.js
@@ -0,0 +1,21 @@
+const secretKey = 'your-secret-key'; // Replace with your actual secret key
+
+export const encodeId = (id) => {
+    const base64Id = Buffer.from(id.toString()).toString('base64');
+    let encoded = '';
+    for (let i = 0; i < base64Id.length; i++) {
+        const charCode = base64Id.charCodeAt(i) ^ secretKey.charCodeAt(i % secretKey.length);
+        encoded += String.fromCharCode(charCode);
+    }
+    return Buffer.from(encoded).toString('base64');
+};
+
+export const decodeId = (encodedId) => {
+    const decoded = Buffer.from(encodedId, 'base64').toString('utf8');
+    let base64Id = '';
+    for (let i = 0; i < decoded.length; i++) {
+        const charCode = decoded.charCodeAt(i) ^ secretKey.charCodeAt(i % secretKey.length);
+        base64Id += String.fromCharCode(charCode);
+    }
+    return Buffer.from(base64Id, 'base64').toString('utf8');
+};
