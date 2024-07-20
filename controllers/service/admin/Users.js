9bb29fd7b33351787246ca417cabe8b615deec0d
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-21 02:43:49 +0700

    get list user, detail user, & add user

--- controllers/service/admin/Users.js
+++ controllers/service/admin/Users.js
@@ -57,10 +57,19 @@ export const getUsers = async (req, res) => {
 
         if (resData.length > 0) {
 
-            const resDatas = resData.map(item => ({
-                ...item.toJSON(),
-                encodedId: encodeId(item.id)
-            }));
+            // const resDatas = resData.map(item => ({
+            //     ...item.toJSON(),
+            //     encodedId: encodeId(item.id)
+            // }));
+
+            const resDatas = resData.map(item => {
+                const jsonItem = item.toJSON();
+                jsonItem.id_ = encodeId(item.id); // Add the encoded ID to the response
+                delete jsonItem.id; // Hapus kolom id dari output JSON
+               
+                return jsonItem;
+            });
+
 
             // Simpan hasil query di Redis cache
             await redisSet(redis_key, JSON.stringify(resDatas), process.env.REDIS_EXPIRE_TIME_SOURCE_DATA);
@@ -83,3 +92,102 @@ export const getUsers = async (req, res) => {
         });
     }
 };
+
+export const getUserById = async (req, res) => {
+    const { id } = req.params; // Ambil id dari params URL
+    try {
+        const resData = await DataUsers.findOne({
+            where: {
+                id: decodeId(id),
+                is_delete: 0
+            },
+            
+        });
+        if(resData != null){
+            
+
+            // res.status(200).json({
+            //     status: 1,
+            //     message: 'Data berhasil ditemukan',
+            //     data: resData
+            // });
+           
+            const data = {
+                id_: id, 
+                ...resData.toJSON(), // Convert Sequelize instance to plain object
+            };
+            delete data.id; // Remove original ID from the response
+
+            res.status(200).json({
+                status: 1,
+                message: 'Data berhasil ditemukan',
+                data: data
+            });
+
+        }else{
+
+            res.status(200).json({
+                'status': 0,
+                'message': 'Data tidak ditemukan',
+            });
+
+        }
+    }catch (error) {
+        res.status(500).json({
+            status: 0,
+            message: error.message,
+        });
+    }
+
+    
+}
+
+export const addUser = async (req, res) => {
+    const { username, email, nama, whatsapp, password, role, sekolah_id, is_active } = req.body;
+
+    // Validate request data
+    if (!username || !email || !nama || !whatsapp || !password) {
+        return res.status(400).json({ status: 0, message: 'Semua kolom harus diisi' });
+    }
+
+    try {
+        // Hash the password before saving it to the database
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // Create a new user record in the database
+        const newUser = await DataUsers.create({
+            username,
+            email,
+            nama,
+            whatsapp,
+            password_: hashedPassword,
+            role_: role,
+            sekolah_id,
+            created_at: new Date(),
+            created_by: req.user.userId, // Use user ID from token
+            created_by_ip: req.ip,
+            is_active // Set the new user as active
+        });
+
+        const data = {
+            id_: encodeId(newUser.id), // Use the virtual id_ field which is already encoded
+            ...newUser.toJSON()
+        };
+        delete data.id; // Remove the original ID from the response
+        delete data.password_ // Remove the original ID from the response
+
+        res.status(200).json({
+            status: 1,
+            message: 'Berhasil tambah pengguna',
+            data: data
+        });
+    } catch (error) {
+        console.error('Gagal tambah pengguna:', error);
+        res.status(500).json({
+            status: 0,
+            message: error.message
+        });
+    }
+};
+
+
