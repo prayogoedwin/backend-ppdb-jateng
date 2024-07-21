3836be57b5a5b9d107c9567e7513a64b16771b5a
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-21 11:32:17 +0700

    role in login

--- controllers/service/admin/Auth.js
+++ controllers/service/admin/Auth.js
@@ -42,20 +42,25 @@ export const loginAdmin = [
             // Check if user exists
             const user = await DataUsers.findOne({
                 where: {
-                    username,
+                    // username,
+                    [Sequelize.Op.or]: [
+                        { username: username },
+                        { email: username },
+                        { whatsapp: username },
+                    ],
                     is_active: 1,
                     is_delete: 0
                 }
             });
 
             if (!user) {
-                return res.status(200).json({ status: 0, message: 'Invalid username or password 1' });
+                return res.status(200).json({ status: 0, message: 'User / Password Salah 1' });
             }
 
             // Compare password
             const isMatch = await bcrypt.compare(password, user.password_);
             if (!isMatch) {
-                return res.status(200).json({ status: 0, message: 'Invalid username or password 2' });
+                return res.status(200).json({ status: 0, message: 'User / Password Salah 2' });
             }
 
             // Generate tokens
@@ -69,11 +74,11 @@ export const loginAdmin = [
 
             res.status(200).json({
                 status: 1,
-                message: 'Login successful',
+                message: 'Berhasil masuk',
                 data: {
                     userId: user.id,
                     username: user.username,
-                    role: user.role,
+                    role: user.role_,
                     accessToken,
                     refreshToken
                 }
@@ -105,7 +110,7 @@ export const logoutAdmin = [
             });
 
             if (!user) {
-                return res.status(400).json({ status: 0, message: 'Invalid user ID' });
+                return res.status(400).json({ status: 0, message: 'Data tidak ditemukan' });
             }
 
             // Invalidate tokens
@@ -115,7 +120,7 @@ export const logoutAdmin = [
 
             res.status(200).json({
                 status: 1,
-                message: 'Logout successful',
+                message: 'Berhasil keluar',
             });
         } catch (error) {
             res.status(500).json({
