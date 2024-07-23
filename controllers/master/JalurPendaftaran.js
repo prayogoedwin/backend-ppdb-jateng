35a2fcd91e6f9184b5d4c3bab3400f9feb6a6d81
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-23 15:52:23 +0700

    jalur pendaftaran & sekolah tujuan

--- controllers/master/JalurPendaftaran.js
+++ controllers/master/JalurPendaftaran.js
@@ -15,6 +15,7 @@ import { redisGet, redisSet } from '../../redis.js'; // Import the Redis functio
 
 export const getJalurPendaftaran = async (req, res) => {
     const redis_key = 'JalurPendaftaranBy_'+req.body.bentuk_pendidikan_id;
+    const nisn = req.body.bentuk_pendidikan_id;
     try {
         const cacheNya = await redisGet(redis_key);
         if (cacheNya) {
--- controllers/master/SekolahTujuan.js
+++ controllers/master/SekolahTujuan.js
@@ -15,6 +15,7 @@ import { redisGet, redisSet } from '../../redis.js'; // Import the Redis functio
 
 export const getSekolahTujuan = async (req, res) => {
     const redis_key = 'SekolahTujuans'+req.body.bentuk_pendidikan_id;
+    const nisn = req.body.bentuk_pendidikan_id;
     try {
         const cacheNya = await redisGet(redis_key);
         if (cacheNya) {
