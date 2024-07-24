6b69f24377faa0fd80349688f25dfa05d4af9176
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-07-24 22:13:34 +0700

    perangkingan

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -1,6 +1,7 @@
 import { check, validationResult } from 'express-validator';
 import DataPendaftars from "../../models/service/DataPendaftarModel.js";
 import DataPerangkingans from "../../models/service/DataPerangkinganModel.js";
+import FileTambahans from "../../models/master/FileTambahanModel.js";
 import multer from "multer";
 import crypto from "crypto";
 import path from "path";
@@ -67,7 +68,7 @@ const calculateAge = (birthdate) => {
 };
 
 // Function to handle POST request
-export const createPerangkingan = async (req, res) => {
+export const cekPerangkingan = async (req, res) => {
         // // Handle validation results
         // const errors = validationResult(req);
         // if (!errors.isEmpty()) {
@@ -85,7 +86,7 @@ export const createPerangkingan = async (req, res) => {
             } = req.body;
 
              // Retrieve data from DataPendaftarModel
-             const pendaftar = await DataPendaftars.findOne({
+            const pendaftar = await DataPendaftars.findOne({
                 where: {
                     id: id_pendaftar,
                     is_delete: 0
@@ -111,46 +112,17 @@ export const createPerangkingan = async (req, res) => {
                 return res.status(200).json({ status: 0, message: 'NISN sudah terdaftar lebih dari 2 kali' });
             }
 
-            const no_pendaftaran = await generatePendaftaranNumber();
+            const data_file_tambahan = await FileTambahans.findAll({
+                where: {
+                    id_jalur_pendaftaran: jalur_pendaftaran_id,
+                    is_active: 1
+                }
+            });
 
-
-
-            // Create a new entry in the ez_perangkingan table
-            // const newPerangkingan = await DataPerangkingans.create({
-            //     id_pendaftar,
-            //     no_pendaftaran,
-            //     bentuk_pendidikan_id,
-            //     jalur_pendaftaran_id,
-            //     sekolah_tujuan_id,
-            //     jurusan_id,
-            //     nisn,
-
-            //     nik: pendaftar.nik,
-            //     nama_lengkap: pendaftar.nama_lengkap,
-            //     tanggal_lahir: new Date(pendaftar.tanggal_lahir),
-            //     umur: calculateAge(pendaftar.tanggal_lahir),
-            //     tahun_lulus: pendaftar.tahun_lulus ? pendaftar.tahun_lulus : 0,
-            //     umur_sertifikat: pendaftar.umur_sertifikat ? pendaftar.umur_sertifikat : 0,
-
-            //     jarak: 20,
-
-            //     nilai_raport: pendaftar.nilai_raport_rata,
-            //     nilai_prestasi: pendaftar.nilai_prestasi,
-            //     nilai_akhir,
-
-            //     is_tidak_sekolah: pendaftar.is_tidak_sekolah,
-            //     is_anak_panti: pendaftar.is_anak_panti,
-            //     is_anak_keluarga_tidak_mampu: pendaftar.is_anak_keluarga_tidak_mampu,
-            //     is_anak_guru_jateng: pendaftar.is_anak_guru_jateng,
-            //     is_pip: pendaftar.is_pip,
-
-            //     created_by: id_pendaftar,
-            //     created_by_ip: req.ip
-            // });
 
             const newPerangkingan = {
                 id_pendaftar,
-                no_pendaftaran,
+                //no_pendaftaran,
                 bentuk_pendidikan_id,
                 jalur_pendaftaran_id,
                 sekolah_tujuan_id,
@@ -180,6 +152,7 @@ export const createPerangkingan = async (req, res) => {
                 created_by_ip: req.ip
             };
 
+
             // Filter the data to be sent as a response
             // const responseData = {
             //     nisn: newPerangkingan.nisn,
@@ -189,16 +162,154 @@ export const createPerangkingan = async (req, res) => {
             // Send success response
             res.status(201).json({
                 status: 1,
-                message: 'Perangkingan berhasil dibuat',
-                data: newPerangkingan
+                message: 'Hasil pengecekan',
+                data: {
+                    ...newPerangkingan.toJSON(),
+                    file_tambahan: data_file_tambahan
+                }
             });
         } catch (error) {
-            console.error('Error perangkingan:', error);
+            console.error('Error pengecekan:', error);
             res.status(500).json({
                 status: 0,
-                message: error.message || 'Terjadi kesalahan saat proses perangkingan'
+                message: error.message || 'Terjadi kesalahan saat proses pengecekan'
             });
         }
+}
+
+// Function to handle POST request
+export const createPerangkingan = async (req, res) => {
+    // // Handle validation results
+    // const errors = validationResult(req);
+    // if (!errors.isEmpty()) {
+    //     return res.status(400).json({ status: 0, errors: errors.array() });
+    // }
+
+    try {
+        const {
+            id_pendaftar,
+            bentuk_pendidikan_id,
+            jalur_pendaftaran_id,
+            sekolah_tujuan_id,
+            jurusan_id,
+            nisn,
+        } = req.body;
+
+         // Retrieve data from DataPendaftarModel
+         const pendaftar = await DataPendaftars.findOne({
+            where: {
+                id: id_pendaftar,
+                is_delete: 0
+            }
+        });
+
+        if (!pendaftar) {
+            return res.status(200).json({ status: 0, message: 'Pendaftar tidak ditemukan' });
+        }
+
+        // Hitung nilai_akhir sebagai penjumlahan dari nilai_raport_rata dan nilai_prestasi
+        const nilai_akhir = (pendaftar.nilai_raport_rata || 0) + (pendaftar.nilai_prestasi || 0);
+
+        // Count existing entries with the same NISN that are not deleted
+        const count = await DataPerangkingans.count({
+            where: {
+                nisn,
+                is_delete: 0
+            }
+        });
+
+        if (count >= 2) {
+            return res.status(200).json({ status: 0, message: 'NISN sudah terdaftar lebih dari 2 kali' });
+        }
+
+        const no_pendaftaran = await generatePendaftaranNumber();
+
+
+
+        // Create a new entry in the ez_perangkingan table
+        // const newPerangkingan = await DataPerangkingans.create({
+        //     id_pendaftar,
+        //     no_pendaftaran,
+        //     bentuk_pendidikan_id,
+        //     jalur_pendaftaran_id,
+        //     sekolah_tujuan_id,
+        //     jurusan_id,
+        //     nisn,
+
+        //     nik: pendaftar.nik,
+        //     nama_lengkap: pendaftar.nama_lengkap,
+        //     tanggal_lahir: new Date(pendaftar.tanggal_lahir),
+        //     umur: calculateAge(pendaftar.tanggal_lahir),
+        //     tahun_lulus: pendaftar.tahun_lulus ? pendaftar.tahun_lulus : 0,
+        //     umur_sertifikat: pendaftar.umur_sertifikat ? pendaftar.umur_sertifikat : 0,
+
+        //     jarak: 20,
+
+        //     nilai_raport: pendaftar.nilai_raport_rata,
+        //     nilai_prestasi: pendaftar.nilai_prestasi,
+        //     nilai_akhir,
+
+        //     is_tidak_sekolah: pendaftar.is_tidak_sekolah,
+        //     is_anak_panti: pendaftar.is_anak_panti,
+        //     is_anak_keluarga_tidak_mampu: pendaftar.is_anak_keluarga_tidak_mampu,
+        //     is_anak_guru_jateng: pendaftar.is_anak_guru_jateng,
+        //     is_pip: pendaftar.is_pip,
+
+        //     created_by: id_pendaftar,
+        //     created_by_ip: req.ip
+        // });
+
+        const newPerangkingan = {
+            id_pendaftar,
+            no_pendaftaran,
+            bentuk_pendidikan_id,
+            jalur_pendaftaran_id,
+            sekolah_tujuan_id,
+            jurusan_id,
+            nisn,
+
+            nik: pendaftar.nik,
+            nama_lengkap: pendaftar.nama_lengkap,
+            tanggal_lahir: new Date(pendaftar.tanggal_lahir),
+            umur: calculateAge(pendaftar.tanggal_lahir),
+            tahun_lulus: pendaftar.tahun_lulus ? pendaftar.tahun_lulus : 0,
+            umur_sertifikat: pendaftar.umur_sertifikat ? pendaftar.umur_sertifikat : 0,
+
+            jarak: 20,
+
+            nilai_raport: pendaftar.nilai_raport_rata,
+            nilai_prestasi: pendaftar.nilai_prestasi,
+            nilai_akhir,
+
+            is_tidak_sekolah: pendaftar.is_tidak_sekolah,
+            is_anak_panti: pendaftar.is_anak_panti,
+            is_anak_keluarga_tidak_mampu: pendaftar.is_anak_keluarga_tidak_mampu,
+            is_anak_guru_jateng: pendaftar.is_anak_guru_jateng,
+            is_pip: pendaftar.is_pip,
+
+            created_by: id_pendaftar,
+            created_by_ip: req.ip
+        };
+
+        // Filter the data to be sent as a response
+        // const responseData = {
+        //     nisn: newPerangkingan.nisn,
+        //     nama_lengkap: newPerangkingan.nama_lengkap
+        // };
+
+        // Send success response
+        res.status(201).json({
+            status: 1,
+            message: 'Perangkingan berhasil dibuat',
+            data: newPerangkingan
+        });
+    } catch (error) {
+        console.error('Error perangkingan:', error);
+        res.status(500).json({
+            status: 0,
+            message: error.message || 'Terjadi kesalahan saat proses perangkingan'
+        });
+    }
 }
 
 
