49b2c894958b05db776087e1bc7a83455211a349
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-09-23 18:34:23 +0700

    pendftr

--- controllers/service/Pendaftar.js
+++ controllers/service/Pendaftar.js
@@ -387,28 +387,6 @@ export const getPendaftarDetail = async (req, res) => {
             });
           }
         }
-
-          // Tambahkan base64 ke file_tambahan
-          let fileTambahanWithBase64 = profil.file_tambahan.map(file => {
-            const filePath = path.join(__dirname, `../upload/berkas/${req.body.nisn}/${file.filename}`);
-            
-            let base64 = '';
-
-            try {
-                const fileBuffer = fs.readFileSync(filePath);
-                base64 = fileBuffer.toString('base64');
-            } catch (err) {
-                console.error(`Error reading file ${file.filename}:`, err);
-            }
-
-            return {
-                ...file,
-                base64: base64
-            };
-        });
-
-        // Masukkan file_tambahan yang sudah di-update dengan base64 ke profil
-        profil.setDataValue('file_tambahan', fileTambahanWithBase64);
   
         // Buat timeline_pendaftar
         const timeline_pendaftar = {
