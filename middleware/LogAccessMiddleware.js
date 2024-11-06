64eb505ae538fba79af7159a4049bc8d473ecb37
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-11-06 15:34:09 +0700

    authHeader on log  middleware

--- middleware/LogAccessMiddleware.js
+++ middleware/LogAccessMiddleware.js
@@ -1,16 +1,31 @@
 // middleware/logAccess.js
 import AccessLog from '../models/AccessLog.js';
-import db from "../config/Database.js";
+import jwt from 'jsonwebtoken';
 
 async function logAccess(req, res, next) {
 
-    const nisn_p = req.params.nisn;
-    const nisn_b = req.body.nisn;
+    // Extract token from the Authorization header
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1]; // Extract token from Bearer
+
+    let userId = null;
+
+    // If there's a token, verify it
+    if (token) {
+        try {
+            const decoded = jwt.verify(token, process.env.JWT_SECRET_KEY);
+            userId = decoded.userId;  // Assuming userId is in the token payload
+        } catch (err) {
+            userId = null;
+            console.error('Token verification failed:', err);
+            return res.status(403).json({ message: 'Invalid token' });
+        }
+    }
 
     try {
         const logData = {
             url: req.originalUrl,
-            akun: nisn_p ? nisn_b : null, // Jika menggunakan autentikasi, ambil dari `req.user`
+            akun: userId, // Jika menggunakan autentikasi, ambil dari `req.user`
             json_data:  null, // Ambil data JSON dari body
             created_at: new Date(),
             created_by: req.user ? req.user.id : null, // ID user jika tersedia
@@ -20,8 +35,8 @@ async function logAccess(req, res, next) {
         // Simpan ke dalam database
         await AccessLog.create(logData);
         console.log("Log entry created:", logData); // 
-
         next(); // Lanjutkan ke handler berikutnya
+
     } catch (error) {
         console.error('Error logging access:', error);
         next(error); // Lanjutkan ke error handler jika ada masalah
