146ee34ca1b771f4ec9c2b6d4fd20267c4d5a2ee
Edwin <you@example.comprayogo.edwin@gmail.com>
2024-11-18 16:58:34 +0700

    sekolah tujuan where zona

--- controllers/master/SekolahTujuan.js
+++ controllers/master/SekolahTujuan.js
@@ -1,5 +1,6 @@
 // Import model Product
 import SekolahTujuans from "../../models/master/SekolahTujuanModel.js";
+import DataPendaftars from "../../models/service/DataPendaftarModel.js";
 import { redisGet, redisSet } from '../../redis.js'; // Import the Redis functions
 
 
@@ -13,7 +14,7 @@ import { redisGet, redisSet } from '../../redis.js'; // Import the Redis functio
 //     }
 // }
 
-export const getSekolahTujuan = async (req, res) => {
+export const getSekolahTujuanX = async (req, res) => {
     const redis_key = 'SekolahTujuans'+req.body.bentuk_pendidikan_id;
     try {
         const cacheNya = await redisGet(redis_key);
@@ -35,6 +36,86 @@ export const getSekolahTujuan = async (req, res) => {
                 },
                 attributes: ['id', 'nama', 'lat', 'lng', 'daya_tampung', 'alamat_jalan'] // Specify the attributes to retrieve
             });
+
+            
+            if(resData.length > 0){
+
+                const newCacheNya = resData;
+                await redisSet(redis_key, JSON.stringify(newCacheNya), process.env.REDIS_EXPIRE_TIME_MASTER); 
+
+                res.status(200).json({
+                    'status': 1,
+                    'message': 'Data berhasil ditemukan',
+                    'data': resData
+                });
+            }else{
+
+                res.status(200).json({
+                    'status': 0,
+                    'message': 'Data kosong',
+                    'data': resData
+                });
+
+            }
+
+        }
+    } catch (err){
+        console.error('Error fetching data:', err); // Log the error for debugging
+        res.status(404).json({
+            'status': 0,
+            'message': 'Error'
+        });
+    }
+}
+
+export const getSekolahTujuan = async (req, res) => {
+    const redis_key = 'SekolahTujuans'+req.body.bentuk_pendidikan_id;
+    try {
+        const cacheNya = await redisGet(redis_key);
+        // const cacheNya = false;
+        if (cacheNya) {
+
+            res.status(200).json({
+                'status': 1,
+                'message': 'Data di ambil dari cache',
+                'data': JSON.parse(cacheNya)
+            });
+
+           
+        }else{
+
+            const nins = req.body.nisn;
+            const jalur_pendaftaran_id = req.body.jalur_pendaftaran_id;
+
+            if(jalur_pendaftaran_id == 1){
+
+                const cekPendaftar = await DataPendaftars.findOne({
+                    where: {
+                        nisn: req.body.nisn,
+                        is_delete: 0
+                    },
+                });
+    
+                const resData = await SekolahTujuans.findAll({
+                    where: {
+                        bentuk_pendidikan_id: req.body.bentuk_pendidikan_id,
+                        kode_wilayah_kec: cekPendaftar.kecamatan_id
+                    },
+                    attributes: ['id', 'nama', 'lat', 'lng', 'daya_tampung', 'alamat_jalan'] // Specify the attributes to retrieve
+                });
+
+            }else{
+
+                const resData = await SekolahTujuans.findAll({
+                    where: {
+                        bentuk_pendidikan_id: req.body.bentuk_pendidikan_id
+                    },
+                    attributes: ['id', 'nama', 'lat', 'lng', 'daya_tampung', 'alamat_jalan'] // Specify the attributes to retrieve
+                });
+
+            }
+
+            
 
             
             if(resData.length > 0){
