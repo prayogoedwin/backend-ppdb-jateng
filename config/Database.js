584f0b68093530c4132ad650f2d7ae5d138062a4
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-01-07 23:44:04 +0700

    update db port

--- config/Database.js
+++ config/Database.js
@@ -7,6 +7,7 @@ dotenv.config();
 
 const db = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: process.env.DB_DRIVER
 });
 
