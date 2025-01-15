83a213cfc088ab97d0e58eaad7e209135f3ba98c
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-01-15 21:53:05 +0700

    smk show jurusan & kuota jurusan

--- controllers/master/SekolahTujuan.js
+++ controllers/master/SekolahTujuan.js
@@ -77,7 +77,7 @@ export const getSekolahTujuanPublik = async (req, res) => {
     }
 }
 
-export const getSekolahTujuan = async (req, res) => {
+export const getSekolahTujuanBAK = async (req, res) => {
 
 
             const nins = req.body.nisn;
@@ -163,6 +163,135 @@ export const getSekolahTujuan = async (req, res) => {
             }
 
 }
+
+export const getSekolahTujuan = async (req, res) => {  
+    const nins = req.body.nisn;  
+    const jalur_pendaftaran_id = req.body.jalur_pendaftaran_id;  
+  
+    try {  
+        if (jalur_pendaftaran_id == 1) {  
+            const cekPendaftar = await DataPendaftars.findOne({  
+                where: {  
+                    nisn: req.body.nisn,  
+                    is_delete: 0  
+                },  
+            });  
+  
+            const resData = await SekolahTujuans.findAll({  
+                where: {  
+                    bentuk_pendidikan_id: req.body.bentuk_pendidikan_id,  
+                    kode_wilayah_kec: cekPendaftar.kecamatan_id  
+                },  
+                attributes: ['id', 'nama', 'npsn', 'lat', 'lng', 'daya_tampung', 'alamat_jalan']  
+            });  
+  
+            // Tambahkan properti nama_npsn ke setiap item dalam resData  
+            const formattedResData = resData.map(school => {  
+                return {  
+                    ...school.dataValues,  
+                    nama_npsn: `${school.nama} ${school.npsn}`  
+                };  
+            });  
+  
+            // If bentuk_pendidikan_id is 15, fetch jurusan data  
+            if (req.body.bentuk_pendidikan_id == 15) {  
+                const jurusanData = await SekolahJurusan.findAll({  
+                    where: {  
+                        id_sekolah_tujuan: formattedResData.map(school => school.id) // Get the ids of the schools  
+                    },  
+                    attributes: ['id', 'nama_jurusan', 'id_sekolah_tujuan', 'daya_tampung']  
+                });  
+  
+                // Format jurusan data  
+                const formattedJurusanData = jurusanData.map(jurusan => {  
+                    return {  
+                        ...jurusan.dataValues,  
+                        id_sekolah_tujuan: jurusan.id_sekolah_tujuan // Include the school id for reference  
+                    };  
+                });  
+  
+                // Combine school and jurusan data  
+                formattedResData.forEach(school => {  
+                    school.jurusan = formattedJurusanData.filter(jurusan => jurusan.id_sekolah_tujuan === school.id);  
+                });  
+            }  
+  
+            if (formattedResData.length > 0) {  
+                res.status(200).json({  
+                    'status': 1,  
+                    'message': 'Data berhasil ditemukan',  
+                    'data': formattedResData  
+                });  
+            } else {  
+                res.status(200).json({  
+                    'status': 0,  
+                    'message': 'Data kosong',  
+                    'data': ''  
+                });  
+            }  
+  
+        } else {  
+            const resData = await SekolahTujuans.findAll({  
+                where: {  
+                    bentuk_pendidikan_id: req.body.bentuk_pendidikan_id  
+                },  
+                attributes: ['id', 'nama', 'npsn', 'lat', 'lng', 'daya_tampung', 'alamat_jalan']  
+            });  
+  
+            // Tambahkan properti nama_npsn ke setiap item dalam resData  
+            const formattedResData = resData.map(school => {  
+                return {  
+                    ...school.dataValues,  
+                    nama_npsn: `${school.nama} ${school.npsn}`  
+                };  
+            });  
+  
+            // If bentuk_pendidikan_id is 15, fetch jurusan data  
+            if (req.body.bentuk_pendidikan_id == 15) {  
+                const jurusanData = await SekolahJurusan.findAll({  
+                    where: {  
+                        id_sekolah_tujuan: formattedResData.map(school => school.id)  
+                    },  
+                    attributes: ['id', 'nama_jurusan', 'id_sekolah_tujuan', 'daya_tampung']  
+                });  
+  
+                // Format jurusan data  
+                const formattedJurusanData = jurusanData.map(jurusan => {  
+                    return {  
+                        ...jurusan.dataValues,  
+                        id_sekolah_tujuan: jurusan.id_sekolah_tujuan  
+                    };  
+                });  
+  
+                // Combine school and jurusan data  
+                formattedResData.forEach(school => {  
+                    school.jurusan = formattedJurusanData.filter(jurusan => jurusan.id_sekolah_tujuan === school.id);  
+                });  
+            }  
+  
+            if (formattedResData.length > 0) {  
+                res.status(200).json({  
+                    'status': 1,  
+                    'message': 'Data berhasil ditemukan',  
+                    'data': formattedResData  
+                });  
+            } else {  
+                res.status(200).json({  
+                    'status': 0,  
+                    'message': 'Data kosong',  
+                    'data': ''  
+                });  
+            }  
+        }  
+    } catch (error) {  
+        console.error(error);  
+        res.status(500).json({  
+            'status': 0,  
+            'message': 'Terjadi kesalahan pada server',  
+            'data': ''  
+        });  
+    }  
+}  
 
 export const getSekolahTujuanJurusanPublik = async (req, res) => {
 
