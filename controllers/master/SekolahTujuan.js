201a7065337b71b27eb9c14b7fe292aed4cabb7f
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-01-23 12:17:52 +0700

    kuota jurusan jarak terdekat dan kuota_afirmasi_persentase

--- controllers/master/SekolahTujuan.js
+++ controllers/master/SekolahTujuan.js
@@ -300,7 +300,7 @@ export const getSekolahTujuanJurusanPublik = async (req, res) => {
                 id_sekolah_tujuan: req.body.id_sekolah_tujuan, // Filter dari EzSekolahTujuans
             },
             // attributes: ['id', 'npsn' ,'nama_jurusan', 'id_jurusan', 'daya_tampung'], // Ambil atribut hanya dari EzSekolahJurusan
-            attributes: ['id', 'nama_jurusan', 'npsn', 'id_jurusan', 'daya_tampung', 'kuota_terdekat_persentase', 'kuota_afrmasi_persentase', 'kuota_prestasi_persentase', 'kuota_prestasi_khusus_persentase'] // Specify the attributes to retrieve
+            attributes: ['id', 'nama_jurusan', 'npsn', 'id_jurusan', 'daya_tampung', 'kuota_jarak_terdekat_persentase', 'kuota_afrmasi_persentase', 'kuota_prestasi_persentase', 'kuota_prestasi_khusus_persentase'] // Specify the attributes to retrieve
         });
 
 
