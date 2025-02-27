36a3f32e3ce0d5d58fdf582c2f42867d1f8f997c
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-02-27 16:20:43 +0700

    siswa log

--- controllers/log/Siswa.js
+++ controllers/log/Siswa.js
@@ -1,116 +1,40 @@
-import DataUsers from '../../../models/service/DataUsersModel.js';
-import { encodeId, decodeId } from '../../../middleware/EncodeDecode.js';
-import { Op } from 'sequelize';
-import { redisGet, redisSet } from '../../../redis.js';
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
-import { clearCacheByKeyFunction } from '../../config/CacheControl.js';
-import RolesData from '../../../models/service/RolesModel.js';
+import AccessLog from '../../../models/AccessLog.js';
+import AccessLogPub from '../../../models/AccessLogPublic.js';
 
-export const getDataPendaftarForVerif = async (req, res) => {
-    const redis_key = 'DataPendaftarAllinAdmin';
+export const LogSiswaLoggedIn = async (req, res) => {
     try {
-        // const cacheNya = await redisGet(redis_key);
-        const cacheNya = false;
-        if (cacheNya) {
+        
+        const resData = await AccessLog.findAll({
+            where: {
+                akun: nisn
+            }
+        });
+
+        const resData2 = await AccessLogPub.findAll({
+            where: {
+                akun: nisn
+            }
+        });
+        
+        if(resData != null){
 
             res.status(200).json({
                 'status': 1,
-                'message': 'Data di ambil dari cache',
-                'data': JSON.parse(cacheNya)
+                'message': 'Data berhasil ditemukan',
+                'data': resData,
+                'other_data': resData2,
             });
-           
+
         }else{
 
-            const adminNya = req.user.userId;
-
-            const dataAdminNya = await DataUsers.findOne({
-                where: {
-                    id: adminNya,
-                    is_active: 1,
-                    is_delete: 0
-                }
+            res.status(200).json({
+                'status': 0,
+                'message': 'Data kosong',
             });
-
-
-            let whereFor = {
-                [Op.or]: [
-                  { is_delete: { [Op.is]: null } },
-                  { is_delete: 0 }
-                ]
-              };
-      
-            if (dataAdminNya.role_ == 101) {
-                whereFor.verifikasikan_disdukcapil = 1
-                whereFor.kabkota_id = dataAdminNya.kabkota_id
-            }
-
-            const resData = await DataPendaftars.findAll({
-                attributes: { exclude: ['password_'] },
-                include: [
-                    {
-                        model: WilayahVerDapodik,
-                        as: 'data_wilayah',
-                        attributes: ['kode_wilayah','nama', 'mst_kode_wilayah']
-                    },
-                    {
-                        model: WilayahVerDapodik,
-                        as: 'data_wilayah_kec',
-                        attributes: ['kode_wilayah', 'nama', 'mst_kode_wilayah']
-                    },
-                    {
-                        model: WilayahVerDapodik,
-                        as: 'data_wilayah_kot',
-                        attributes: ['kode_wilayah', 'nama', 'mst_kode_wilayah']
-                    },
-                    {
-                        model: DataUsers,
-                        as: 'diverifikasi_oleh',
-                        attributes: ['id', 'nama']
-                    }
-                ],
-                where: whereFor
-            });
-            if(resData != null){
-
-                // const resDatas = resData.map(item => ({
-                //     ...item.toJSON(),
-                //     encodedId: encodeId(item.id)
-                // }));
-                const resDatas = resData.map(item => {
-                    const jsonItem = item.toJSON();
-                    jsonItem.id_ = encodeId(item.id); // Add the encoded ID to the response
-                    delete jsonItem.id; // Hapus kolom id dari output JSON
-                   
-                    return jsonItem;
-                });
-
-                // const resDatas = resData.map(item => {
-                //     const jsonItem = item.toJSON();
-                //     const encodedId = encodeId(jsonItem.id); // Encode the original ID
-                //     delete jsonItem.id; // Remove the original ID from the response
-                //     jsonItem.encodedId = encodedId; // Add the encoded ID to the response
-                //     return jsonItem;
-                // });
-
-                const newCacheNya = resDatas;
-                await redisSet(redis_key, JSON.stringify(newCacheNya), process.env.REDIS_EXPIRE_TIME_SOURCE_DATA); 
-
-                res.status(200).json({
-                    'status': 1,
-                    'message': 'Data berhasil ditemukan',
-                    'data': resDatas
-                });
-            }else{
-
-                res.status(200).json({
-                    'status': 0,
-                    'message': 'Data kosong',
-                });
-
-            }
 
         }
+
+
     } catch (err){
         console.error('Error fetching data:', err); // Log the error for debugging
         res.status(404).json({
