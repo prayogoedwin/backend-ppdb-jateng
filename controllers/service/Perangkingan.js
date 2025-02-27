e44e4327b61804d96f2a574493b42aa1ae2bdb3b
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-02-27 19:01:26 +0700

    test download

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -1642,22 +1642,23 @@ export const getPerangkingan = async (req, res) => {
                                 { text: 'Perangkingan Pendaftaran', style: 'header' },
                                 { text: `Jalur Pendaftaran: ${jalur_pendaftaran_id}`, style: 'subheader' },
                                 { text: 'Data Perangkingan:', style: 'subheader' },
-                                // {
-                                //     table: {
-                                //         widths: ['auto', '*', '*', '*', '*', '*'],
-                                //         body: [
-                                //             ['No', 'ID Pendaftar', 'Nama', 'Nilai Akhir', 'Umur', 'Tanggal Daftar'],
-                                //             ...modifiedData.map((item, index) => [
-                                //                 index + 1,
-                                //                 item.id_pendaftar,
-                                //                 item.nama,
-                                //                 item.nilai_akhir,
-                                //                 item.umur,
-                                //                 item.created_at
-                                //             ])
-                                //         ]
-                                //     }
-                                // }
+                                {
+                                    table: {
+                                        widths: ['auto', '*', '*', '*', '*', '*'],
+                                        body: [
+                                            ['No', 'ID Pendaftar', 'Nama', 'Nilai Akhir', 'Umur', 'Tanggal Daftar'],
+                                            ...modifiedData.map((item, index) => [
+                                                index + 1,
+                                                item.no_pendaftaran,
+                                                item.nama_lengkap,
+                                                item.jarak,
+                                                item.nilai_akhir,
+                                                item.umur,
+                                                item.created_at
+                                            ])
+                                        ]
+                                    }
+                                }
                             ],
                             styles: {
                                 header: {
