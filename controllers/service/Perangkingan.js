be651af036c075f21b652e7127a2c23bd48590b6
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-02-27 18:53:01 +0700

    download test

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -1642,22 +1642,22 @@ export const getPerangkingan = async (req, res) => {
                                 { text: 'Perangkingan Pendaftaran', style: 'header' },
                                 { text: `Jalur Pendaftaran: ${jalur_pendaftaran_id}`, style: 'subheader' },
                                 { text: 'Data Perangkingan:', style: 'subheader' },
-                                {
-                                    table: {
-                                        widths: ['auto', '*', '*', '*', '*', '*'],
-                                        body: [
-                                            ['No', 'ID Pendaftar', 'Nama', 'Nilai Akhir', 'Umur', 'Tanggal Daftar'],
-                                            ...modifiedData.map((item, index) => [
-                                                index + 1,
-                                                item.id_pendaftar,
-                                                item.nama,
-                                                item.nilai_akhir,
-                                                item.umur,
-                                                item.created_at
-                                            ])
-                                        ]
-                                    }
-                                }
+                                // {
+                                //     table: {
+                                //         widths: ['auto', '*', '*', '*', '*', '*'],
+                                //         body: [
+                                //             ['No', 'ID Pendaftar', 'Nama', 'Nilai Akhir', 'Umur', 'Tanggal Daftar'],
+                                //             ...modifiedData.map((item, index) => [
+                                //                 index + 1,
+                                //                 item.id_pendaftar,
+                                //                 item.nama,
+                                //                 item.nilai_akhir,
+                                //                 item.umur,
+                                //                 item.created_at
+                                //             ])
+                                //         ]
+                                //     }
+                                // }
                             ],
                             styles: {
                                 header: {
@@ -1682,7 +1682,7 @@ export const getPerangkingan = async (req, res) => {
                             res.setHeader('Content-Disposition', 'attachment; filename=perangkingan.pdf');
                             res.send(buffer);
                         });
-                        
+
                     }else{
 
                         res.status(200).json({
