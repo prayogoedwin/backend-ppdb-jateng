2fcfc7f079d1a35c4db269ff4365289f011434aa
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-03-05 15:48:26 +0700

    rekap admin

--- controllers/service/admin/RekapAdmin.js
+++ controllers/service/admin/RekapAdmin.js
@@ -1,8 +1,10 @@
 import DataPendaftars from "../../../models/service/DataPendaftarModel.js";
 import DataPerangkingans from "../../../models/service/DataPerangkinganModel.js";
+import JalurPendaftarans from "../../../models/master/JalurPendaftaranModel.js";
 import { redisGet, redisSet } from '../../../redis.js';
 import { clearCacheByKeyFunction } from '../../config/CacheControl.js';
-import { Op } from 'sequelize';
+import db from '../../../config/Database.js'; // Sesuaikan path berdasarkan struktur folder
+import { Op, fn, col, QueryTypes } from 'sequelize';
 
 export const countPendaftar = async (req, res) => {
   const sekolah_id = req.params.sekolah_id
@@ -30,6 +32,20 @@ export const countPendaftar = async (req, res) => {
         }
       });
 
+      const genderCounts = await DataPendaftars.findAll({
+        attributes: [
+            'jenis_kelamin',
+            [fn('COUNT', col('jenis_kelamin')), 'rekap_jenis_kelamin']
+        ],
+        where: {
+            [Op.or]: [
+                { is_delete: { [Op.is]: null } },
+                { is_delete: 0 }
+            ]
+        },
+        group: ['jenis_kelamin']
+    });
+
       // Count the verified pendaftar
       const verifiedCount = await DataPendaftars.count({
         where: {
@@ -52,6 +68,28 @@ export const countPendaftar = async (req, res) => {
         }
       });
 
+       // Count the activated pendaftar
+       const dikirimDukcapilCount = await DataPendaftars.count({
+        where: {
+          [Op.or]: [
+            { is_delete: { [Op.is]: null } },
+            { is_delete: 0 }
+          ],
+          verifikasikan_disdukcapil: 1
+        }
+      });
+
+       // Count the activated pendaftar
+       const verifDukcapilCount = await DataPendaftars.count({
+        where: {
+          [Op.or]: [
+            { is_delete: { [Op.is]: null } },
+            { is_delete: 0 }
+          ],
+          is_verified_disdukcapil: 1
+        }
+      });
+
 
        let wherePerangkinganSmaCount = {
           [Op.or]: [
@@ -65,19 +103,10 @@ export const countPendaftar = async (req, res) => {
           wherePerangkinganSmaCount.sekolah_tujuan_id = sekolah_id;
         }
 
+        //statistik pendaftara sekolah
         const perangkinganSmaCount = await DataPerangkingans.count({
           where: wherePerangkinganSmaCount
         });
-        // const perangkinganSmaCount = await DataPerangkingans.count({
-        //   where: {
-        //     [Op.or]: [
-        //       { is_delete: { [Op.is]: null } },
-        //       { is_delete: 0 }
-        //     ],
-        //     bentuk_pendidikan_id: 13
-        //   }
-        // });
-
       
         let wherePerangkinganSmkCount = {
           [Op.or]: [
@@ -94,25 +123,80 @@ export const countPendaftar = async (req, res) => {
         const perangkinganSmkCount = await DataPerangkingans.count({
           where: wherePerangkinganSmkCount
         });
-      
-       // Count the total pendaftar
-      //  const perangkinganSmkCount = await DataPerangkingans.count({
-      //   where: {
-      //     [Op.or]: [
-      //       { is_delete: { [Op.is]: null } },
-      //       { is_delete: 0 }
-      //     ],
-      //     bentuk_pendidikan_id: 15
-      //   }
-      // });
 
-      // Structure the result
+
+        //ini untuk perangkingan query manual
+
+        let sekolahFilter = ""; // Default tanpa filter
+
+        if (sekolah_id != 0) {
+            sekolahFilter = `WHERE sekolah_tujuan_id = ${sekolah_id}`; 
+        }
+
+        const querybentukPendidikan = `
+            SELECT 
+                SUM(CASE 
+                    WHEN jalur_pendaftaran_id IN (1, 2, 3, 4, 5) THEN 1 
+                    ELSE 0 
+                END) AS jumlah_sma,
+                SUM(CASE 
+                    WHEN jalur_pendaftaran_id IN (6, 7, 8, 9) THEN 1 
+                    ELSE 0 
+                END) AS jumlah_smk
+            FROM ez_perangkingan
+             ${sekolahFilter};
+        `;
+
+        const [bentukPendidikan] = await db.query(querybentukPendidikan, { raw: true, type: db.QueryTypes.SELECT });
+
+        //jalur
+        const queryjaluePendaftaranSMA = `
+            SELECT 
+                jp.id AS jalur_pendaftaran_id,
+                jp.nama,
+                COALESCE(COUNT(p.id), 0) AS jumlah_pendaftar
+            FROM ez_jalur_pendaftaran jp
+            LEFT JOIN ez_perangkingan p ON jp.id = p.jalur_pendaftaran_id
+           WHERE jp.id IN (1, 2, 3, 4, 5)
+            GROUP BY jp.id, jp. nama
+            ORDER BY jp.id;
+        `;
+
+        const jaluePendaftaranSMA = await db.query(queryjaluePendaftaranSMA, { 
+          raw: true, 
+          type: db.QueryTypes.SELECT 
+      });
+
+      const queryjaluePendaftaranSMK = `
+          SELECT 
+              jp.id AS jalur_pendaftaran_id,
+              jp.nama,
+              COALESCE(COUNT(p.id), 0) AS jumlah_pendaftar
+          FROM ez_jalur_pendaftaran jp
+          LEFT JOIN ez_perangkingan p ON jp.id = p.jalur_pendaftaran_id
+        WHERE jp.id IN (6, 7, 8, 9)
+          GROUP BY jp.id, jp. nama
+          ORDER BY jp.id;
+      `;
+
+      const jaluePendaftaranSMK = await db.query(queryjaluePendaftaranSMK, { 
+        raw: true, 
+        type: db.QueryTypes.SELECT 
+      });
+
       const result = {
         pendaftar: pendaftarCount,
+        pendaftar_jenis_kelamin: genderCounts,
         pendaftar_terverifikasi: verifiedCount,
         pendaftar_aktivasi: activatedCount,
+        pendaftar_dikirim_kecapil: dikirimDukcapilCount,
+        pendaftar_diverif_capil: verifDukcapilCount,
         daftar_sma: perangkinganSmaCount,
         daftar_smk: perangkinganSmkCount,
+        jenjang_pendidikan: bentukPendidikan,
+        jalur_pendaftaran_sma: jaluePendaftaranSMA, 
+        jalur_pendaftaran_smk: jaluePendaftaranSMK
+     
       };
 
       // Store the result in Redis cache
@@ -134,3 +218,5 @@ export const countPendaftar = async (req, res) => {
     });
   }
 };
+
+
