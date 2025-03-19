b4a4a7671e6e0b32d9487ee13b3b5d294742e4bc
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-03-19 09:26:29 +0700

    kembalikan kode verifikasi

--- controllers/service/PesertaDidik.js
+++ controllers/service/PesertaDidik.js
@@ -305,7 +305,8 @@ export const getPesertaDidikByNisnHandler = async (req, res) => {
         if (cekPendaftar) {
             return res.status(200).json({
                 status: 2,
-                message: 'NISN Sudah Terdaftar Sebelumnya'
+                message: 'NISN Sudah Terdaftar Sebelumnya',
+                data: cekPendaftar.kode_verifikasi
             });
         }
 
