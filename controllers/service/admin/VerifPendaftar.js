0ff5e573aa67cd7455ab17e2b6a2b237392bc5ff
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-03-25 14:11:29 +0700

    test tanpa nilai_raport_def

--- controllers/service/admin/VerifPendaftar.js
+++ controllers/service/admin/VerifPendaftar.js
@@ -1634,7 +1634,7 @@ export const updatePendaftarByUser = async (req, res) => {
                 umur_sertifikat: umur_sertifikat || 0,
                 nomor_sertifikat,
                 nilai_prestasi,
-                nilai_raport: nilai_raport || nilai_raport_def,
+                nilai_raport: nilai_raport_def,
                 nilai_raport_rata,
                 is_tidak_sekolah,
                 is_anak_panti,
