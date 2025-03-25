9a9fd2a2c28c12172d5a463ae721a59007be592d
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-03-25 16:50:57 +0700

    test

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -4981,29 +4981,6 @@ export const createPerangkingan = async (req, res) => {
             return res.status(200).json({ status: 0, message: 'Status anda sedang diminta untuk revisi, tidak dapat mendaftar sekolah sekarang!' });
         }
 
-         //jika status domisili "Menggunakan Surat Perpindahan Tugas Ortu/Wali" maka
-         if(pendaftar.status_domisili == 2){
-            //tidak boleh daftar jalur selain jalur mutasi dan domisili terdekat di SMK
-            if(jalur_pendaftaran_id != 4){
-                return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah "Menggunakan Surat Perpindahan Tugas Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi' });
-            }
-        }
-
-         //jika status domisili bukan "Menggunakan Surat Perpindahan Tugas Ortu/Wali" maka
-         if(pendaftar.status_domisili != 2){
-            if(jalur_pendaftaran_id == 4){
-             return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah `bukan` "Menggunakan Surat Perpindahan Tugas Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi jalur mutasi' });
-            }
-        }
-
-        if(pendaftar.is_tidak_sekolah != 1 || pendaftar.is_anak_panti != 1 || pendaftar.is_anak_keluarga_tidak_mampu != 1){
-
-            if(jalur_pendaftaran_id == 5 || jalur_pendaftaran_id == 9){
-                return res.status(200).json({ status: 0, message: 'Anda tidak bisa mendaftar jalur ini karena anda tidak termasuk salah satu dari kategori afirmasi: (Anak tidak sekolah, anak panti, atau anak dari keluarga tidak mampu yang terdaftar  pada BDT Jateng)' });
-            }
-
-        }
-
         // Hitung nilai_akhir sebagai penjumlahan dari nilai_raport_rata dan nilai_prestasi
         const nilai_akhir = (pendaftar.nilai_raport_rata || 0) + (pendaftar.nilai_prestasi || 0);
 
