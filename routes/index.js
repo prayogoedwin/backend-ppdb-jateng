f909e44206c5f60218fd5242ef5a383d907c0bbe
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-10 16:41:07 +0700

    versi baru

--- routes/index.js
+++ routes/index.js
@@ -329,7 +329,7 @@ router.post('/client-api/external/insert_sertifikat', ipWhitelistMiddleware, app
 
 
 // Define the version as a constant
-const VERSION = '1.20.14'; 
+const VERSION = '1.20.15'; 
 const APPNAME = 'Backend PPDB';
 
 // // Create a GET route at '/' that sends the version as a JSON response
