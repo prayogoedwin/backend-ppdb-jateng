e4f0f3f4c22dbba448be74e556f9ff7a55746059
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-26 22:56:26 +0700

    cors

--- index.js
+++ index.js
@@ -37,7 +37,14 @@ const corsOptions = {
   credentials: true,               // ini wajib supaya cookie/token ikut
 };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3002');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token');
+  next();
+});
 
 app.use(Router);
 
