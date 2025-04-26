bdc89dc0bbf57c8893216f872c82fe82c51f1e8c
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-26 22:17:54 +0700

    validasi stop

--- middleware/validasiPendaftar.js
+++ middleware/validasiPendaftar.js
@@ -16,29 +16,29 @@ const validatePendaftar = [
     .withMessage('NIK harus berupa angka'),
 
     // Validasi sekolah_asal_id: hanya boleh 1 atau 2
-    check('sekolah_asal_id')
-     .isIn([1, 2])
-     .withMessage('kode sekolah asal tidak diketahui'),
+    // check('sekolah_asal_id')
+    //  .isIn([1, 2])
+    //  .withMessage('kode sekolah asal tidak diketahui'),
 
-      // Validasi sjenis lulusan
-    check('jenis_lulusan_id')
-     .isIn([1, 2])
-     .withMessage('kode jenis lulusan tidak diketahui'),
+    //   // Validasi sjenis lulusan
+    // check('jenis_lulusan_id')
+    //  .isIn([1, 2])
+    //  .withMessage('kode jenis lulusan tidak diketahui'),
 
-    // Validasi status domisili
-    check('status_domisili')
-      .isIn([1, 2, 3, 4])
-      .withMessage('kode status domisili tidak diketahui'),
+    // // Validasi status domisili
+    // check('status_domisili')
+    //   .isIn([1, 2, 3, 4])
+    //   .withMessage('kode status domisili tidak diketahui'),
     
-    check('is_tidak_sekolah')
-      .optional({ nullable: true }) // boleh null
-      .isIn(['0', '1']) // atau nilainya harus '0' atau '1'
-      .withMessage('kode status anak tidak sekolah tidak di ketahui'),
+    // check('is_tidak_sekolah')
+    //   .optional({ nullable: true }) // boleh null
+    //   .isIn(['0', '1']) // atau nilainya harus '0' atau '1'
+    //   .withMessage('kode status anak tidak sekolah tidak di ketahui'),
 
-    check('organisasi_id')
-      .optional({ nullable: true }) // boleh null
-      .isIn([0, 1, 2])
-      .withMessage('kode organisasi tidak diketahui'),
+    // check('organisasi_id')
+    //   .optional({ nullable: true }) // boleh null
+    //   .isIn([0, 1, 2])
+    //   .withMessage('kode organisasi tidak diketahui'),
 
 ];
 
