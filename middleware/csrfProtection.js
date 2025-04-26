219c9874880cb7427ed5c1f83d998c3d682a9861
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-26 18:37:40 +0700

    csrfProtection

--- middleware/csrfProtection.js
+++ middleware/csrfProtection.js
@@ -1,14 +1,14 @@
 import csrf from 'csurf';
 
-// const csrfProtection = csrf({ cookie: true });
+const csrfProtection = csrf({ cookie: true });
 
-const csrfProtection = csrf({
-    cookie: {
-      httpOnly: true, // cookie tidak bisa diakses dari Javascript di browser
-      //sameSite: 'strict', // hanya kirim cookie ke origin yang sama
-      //maxAge: 24 * 60 * 60, // cookie expired dalam 1 hari (dalam detik)
-      maxAge: 1, // cookie expired dalam 1 detik
-    }
-  });
+// const csrfProtection = csrf({
+//     cookie: {
+//       httpOnly: true, // cookie tidak bisa diakses dari Javascript di browser
+//       //sameSite: 'strict', // hanya kirim cookie ke origin yang sama
+//       //maxAge: 24 * 60 * 60, // cookie expired dalam 1 hari (dalam detik)
+//       maxAge: 1, // cookie expired dalam 1 detik
+//     }
+//   });
 
 export default csrfProtection;
