995a9dc1133081086a914795b1e291116b10f522
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-26 22:57:33 +0700

    cors back

--- index.js
+++ index.js
@@ -32,19 +32,8 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(cookieParser()); // <-- HARUS sebelum csrfProtection
 
-const corsOptions = {
-  origin: 'http://localhost:3002', // frontend URL kamu
-  credentials: true,               // ini wajib supaya cookie/token ikut
-};
-
-// app.use(cors(corsOptions));
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'http://localhost:3002');
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization, X-CSRF-Token');
-  next();
-});
+// use cors
+app.use(cors());
 
 app.use(Router);
 
