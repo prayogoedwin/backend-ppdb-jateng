14f7790f81f832cc026e4cbf0bd1232588581afc
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-04-28 14:28:47 +0700

    capctha

--- middleware/cloudflareCaptcha.js
+++ middleware/cloudflareCaptcha.js
@@ -3,41 +3,48 @@ import axios from 'axios';
 // Validator Turnstile (reusable)
 // async function validateTurnstile(req, res, next) {
 export const verifyCluodflareCaptcha = async (req, res, next) => {
-    const token = req.body.turnstileToken;
-    // if (!token) return res.status(400).json({ error: 'Token required' });
-    if (!token) {
-        // return res.status(400).json({ error: 'reCAPTCHA token diperlukan' });
-        return res.status(400).json({ 
-            status: 0,
-            message: 'error: turnstile CAPTCHA token diperlukan' 
-          });
-    }
 
-    // const result = await axios.post(
-    //     'https://challenges.cloudflare.com/turnstile/v0/siteverify',
-    //     new URLSearchParams({
-    //       secret: '6LcuUCYrAAAAAPbXRyNZA18berpj6V2nRfMWNAEb', // Ganti dengan secret key reCAPTCHA v3 Anda
-    //       response: recaptchaToken,
-    //     }),
-    //     { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }
-    //   );
-  
-    const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
-      method: 'POST',
-      body: JSON.stringify({
-        secret: "0x4AAAAAABVkMumzcgp09oPqozVWkoK0IeI",
-        response: token,
-        remoteip: req.ip
-      })
-    });
-    
-    const data = await result.json();
-    // if (!data.success) return res.status(403).json({ error: 'Verifikasi gagal' });
-    if (!data.success) {
-        return res.status(403).json({ 
-          status: 0,
-          message: 'Verifikasi turnstile CAPTCHA gagal, silahkan hard refresh browser anda atau ganti browser chrome / mozila terbaru' ,
+    try {
+        const token = req.body.turnstileToken;
+
+        if (!token) {
+            // return res.status(400).json({ error: 'reCAPTCHA token diperlukan' });
+            return res.status(400).json({ 
+                status: 0,
+                message: 'error: turnstile CAPTCHA token diperlukan' 
+            });
+        }
+
+        const result = await fetch('https://challenges.cloudflare.com/turnstile/v0/siteverify', {
+        method: 'POST',
+        body: JSON.stringify({
+            secret: "0x4AAAAAABVkMumzcgp09oPqozVWkoK0IeI",
+            response: token,
+            remoteip: req.ip
+        })
         });
+        
+        const data = await result.json();
+        // if (!data.success) return res.status(403).json({ error: 'Verifikasi gagal' });
+        if (!data.success) {
+            return res.status(403).json({ 
+            status: 0,
+            message: 'Verifikasi turnstile CAPTCHA gagal, silahkan hard refresh browser anda atau ganti browser chrome / mozila terbaru' ,
+            });
+        }
+        next();
+    } catch (error) {
+        if (error.code === 'ECONNABORTED') {
+            return res.status(500).json({ 
+                status: 0,
+                message: 'Turnstile Captca timeout' ,
+            });
+             console.error('Turnstile timeout');
+        }
+        // return res.status(500).json({ 
+        //     status: 0,
+        //     message: 'Turnstile Captca service unavailable' ,
+        // });
+        throw new Error('Turnstile service unavailable');
       }
-    next();
   }
