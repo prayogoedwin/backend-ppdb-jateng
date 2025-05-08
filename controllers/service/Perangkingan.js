ffd81750b31f250672a22ff8adbf9337b7f31e6c
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-05-08 15:42:38 +0700

    cek

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -8363,6 +8363,10 @@ export const cekPerangkingan = async (req, res) => {
             if(jalur_pendaftaran_id == 4){
              return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah `bukan` "Menggunakan Surat Mutasi Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi jalur mutasi (SMA)' });
             }
+
+            if(jalur_pendaftaran_id == 5 || jalur_pendaftaran_id == 9){
+                return res.status(200).json({ status: 0, message: 'Anda tidak bisa mendaftar jalur ini karena anda tidak termasuk salah satu dari kategori afirmasi: (ATS, Anak Panti, Anak Keluarga Tidak Mampu yang terdaftar  pada BDT Jateng)' });
+            }
         }
 
         // if(pendaftar.is_anak_keluarga_tidak_mampu == 0 && jalur_pendaftaran_id == 5){
