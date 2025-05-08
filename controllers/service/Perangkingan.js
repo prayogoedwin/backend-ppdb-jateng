da2586348b69941c47395ba7e32a55898cb13a98
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-05-08 16:30:07 +0700

    good

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -8319,7 +8319,7 @@ export const cekPerangkingan = async (req, res) => {
             attributes: ['id', 'status_domisili', 'kecamatan_id', 'nisn', 'nama_lengkap', 'lat', 'lng',
                  'is_tidak_sekolah', 'is_anak_panti', 'is_anak_keluarga_tidak_mampu', 'is_anak_pondok', 
                  'is_tidak_boleh_domisili', 'tanggal_lahir',
-                 'nilai_raport_rata', 'nilai_prestasi', 'nilai_organisasi', 
+                 'nilai_raport_rata', 'nilai_prestasi', 'nilai_organisasi', 'is_disabilitas', 'is_anak_guru_jateng'
                 ] 
         });
 
@@ -8364,16 +8364,16 @@ export const cekPerangkingan = async (req, res) => {
  
 
             // return res.status(200).json({ status: 0, message: 'AA Anda tidak bisa mendaftar jalur ini karena anda tidak termasuk salah satu dari kategori afirmasi: (ATS, Anak Panti, Anak Keluarga Tidak Mampu yang terdaftar  pada BDT Jateng)' });
-            // if(jalur_pendaftaran_id == 4){
-            // // return res.status(200).json({ status: 0, message: 'AA' });
-            //  return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah `bukan` "Menggunakan Surat Mutasi Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi jalur mutasi (SMA)' });
-            // }
+            if(jalur_pendaftaran_id == 4){
+            // return res.status(200).json({ status: 0, message: 'AA' });
+             return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah `bukan` "Menggunakan Surat Mutasi Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi jalur mutasi (SMA)' });
+            }
 
             if(pendaftar.is_tidak_sekolah == 0 && pendaftar.is_anak_panti == 0 && pendaftar.is_anak_keluarga_tidak_mampu == 0 && pendaftar.is_disabilitas == 0){
-                return res.status(200).json({ status: 0, message: 'BB' });
-                // if(jalur_pendaftaran_id == '5' || jalur_pendaftaran_id == '9'){
-                //     return res.status(200).json({ status: 0, message: '1 Anda tidak bisa mendaftar jalur ini karena anda tidak termasuk salah satu dari kategori afirmasi: (ATS, Anak Panti, Anak Keluarga Tidak Mampu yang terdaftar  pada BDT Jateng)' });
-                // }
+                // return res.status(200).json({ status: 0, message: 'BB' });
+                if(jalur_pendaftaran_id == 5 || jalur_pendaftaran_id == 9){
+                    return res.status(200).json({ status: 0, message: '1 Anda tidak bisa mendaftar jalur ini karena anda tidak termasuk salah satu dari kategori afirmasi: (ATS, Anak Panti, Anak Keluarga Tidak Mampu yang terdaftar  pada BDT Jateng)' });
+                }
     
             }
         }
