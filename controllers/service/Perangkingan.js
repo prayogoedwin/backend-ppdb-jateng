6bf524effde67a13696ba29f3bb810b610807ff4
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-05-15 11:06:26 +0700

    mutasi boleh daftar jaarak terdekat smk

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -10226,8 +10226,8 @@ export const cekPerangkingan = async (req, res) => {
 
         //jika status domisili "Menggunakan Surat Perpindahan Tugas Ortu/Wali" maka
         if(pendaftar.status_domisili == 2){
-            //tidak boleh daftar jalur selain jalur mutasi dan domisili terdekat di SMK
-            if(jalur_pendaftaran_id != 4 && jalur_pendaftaran_id != 3){
+            //tidak boleh daftar jalur selain jalur mutasi dan prestasi, prestasi smk
+            if(jalur_pendaftaran_id != 4 && jalur_pendaftaran_id != 3 && jalur_pendaftaran_id != 6){
                 return res.status(200).json({ status: 0, message: 'Saat ini sistem membaca bahwa status domisili anda adalah "Sesuai Surat Mutasi Ortu/Wali" status domisili tersebut hanya di perbolehkan mendaftar jalur mutasi dan prestasi (SMA)' });
             }
 
