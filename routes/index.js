3fdbf3e685d9de0ad20a3a133ce9adc4312164e4
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-18 03:28:33 +0700

    2.1.11

--- routes/index.js
+++ routes/index.js
@@ -536,7 +536,7 @@ router.get('/rekap-api/monitoring/sma', getMonitoringSMA);
 
 
 // Define the version as a constant
-const VERSION = '2.1.10'; 
+const VERSION = '2.1.11'; 
 const APPNAME = 'Backend PPDB';
 
 // // Create a GET route at '/' that sends the version as a JSON response
