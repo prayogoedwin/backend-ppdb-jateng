0674d6e5eebb6b85918a843f05a3e328a2a71883
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-18 01:22:13 +0700

    1970-01-01 = null date

--- controllers/service/admin/VerifPendaftar.js
+++ controllers/service/admin/VerifPendaftar.js
@@ -3744,7 +3744,7 @@ export const updatePendaftarHapusPerangkingan = async (req, res) => {
                 kejuaraan_id: 0,
                 nama_kejuaraan: 0,
                 umur_sertifikat: 0,
-                tanggal_sertifikat: null,
+                tanggal_sertifikat: '1970-01-01',
                 nomor_sertifikat: 0,
                 nilai_prestasi: 0,
                 updated_at: new Date(),
