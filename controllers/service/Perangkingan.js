afb86a4e968106e7b034c83bb5ee64ab0343a4b7
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-17 11:07:35 +0700

    bug code

--- controllers/service/Perangkingan.js
+++ controllers/service/Perangkingan.js
@@ -13238,11 +13238,14 @@ export const getPerangkingan = async (req, res) => {
                         jurusan_id,
                         is_delete: 0,
                         is_daftar_ulang: { [Op.ne]: 2 }, // Adding the new condition
-                        is_anak_guru_jateng: { [Op.ne]: '1' },
+                        // is_anak_guru_jateng: { [Op.ne]: '1' },
                         // is_tidak_boleh_domisili: { [Op.ne]: '1' },
                         // is_tidak_boleh_domisili: { [Op.not]: '1' }
                         // Alternatif (lebih panjang, tidak disarankan)
-                        }
+                        [Op.or]: [
+                            { is_tidak_boleh_domisili: { [Op.is]: null } },
+                            { is_tidak_boleh_domisili: 0 }
+                        ],
                         nisn: { [Op.notIn]: excludedNisn } // Exclude NISN yang sudah terpilih di anak guru
                     }, order: [
                         [literal('CAST(jarak AS FLOAT)'), 'ASC'], // Use literal for raw SQL  
