c27b78f9ddeaa3a4163a4651c99d5990fec513cb
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-18 02:29:38 +0700

    2.1.10

--- routes/index.js
+++ routes/index.js
@@ -530,7 +530,7 @@ router.post('/client-api/external/insert_sertifikat', ipWhitelistMiddleware, app
 router.get('/rekap-api/external/rekap_harian', pendaftarHarian);
 
 // Define the version as a constant
-const VERSION = '2.1.9'; 
+const VERSION = '2.1.10'; 
 const APPNAME = 'Backend PPDB';
 
 // // Create a GET route at '/' that sends the version as a JSON response
