b738d578fd166cd2b6412bab70922a47630c8713
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-18 09:26:13 +0700

    2.1.13 update keamanan data

--- routes/index.js
+++ routes/index.js
@@ -536,7 +536,7 @@ router.get('/rekap-api/monitoring/sma', getMonitoringSMA);
 
 
 // Define the version as a constant
-const VERSION = '2.1.12'; 
+const VERSION = '2.1.13'; 
 const APPNAME = 'Backend PPDB';
 
 // // Create a GET route at '/' that sends the version as a JSON response
