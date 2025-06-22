420dc82226d68f4b5aa5bd017b8c1e48bd5f2a00
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-06-22 14:26:57 +0700

    melengkapi data pendaftar

--- models/service/DataPerangkinganModel.js
+++ models/service/DataPerangkinganModel.js
@@ -288,8 +288,8 @@ const PerangkinganModels = db.define('ez_perangkingan', {
 export default PerangkinganModels;
 PerangkinganModels.belongsTo(SekolahTujuan, { as: 'sekolah_tujuan', foreignKey: 'sekolah_tujuan_id' });
 PerangkinganModels.belongsTo(JalurPendaftarans, { as: 'jalur_pendaftaran', foreignKey: 'jalur_pendaftaran_id' });
-PerangkinganModels.belongsTo(DataPendaftars, { as: 'data_pendaftar', foreignKey: 'id_pendaftar' });
-PerangkinganModels.belongsTo(DataPendaftars, { as: 'data_pendaftar_sekolah', foreignKey: 'nisn', targetKey: 'nisn' });
+PerangkinganModels.belongsTo(DataPendaftars, { as: 'data_pendaftar_byid', foreignKey: 'id_pendaftar' });
+PerangkinganModels.belongsTo(DataPendaftars, { as: 'data_pendaftar', foreignKey: 'nisn', targetKey: 'nisn' });
 PerangkinganModels.belongsTo(SekolahJurusan, { as: 'sekolah_jurusan', foreignKey: 'jurusan_id' });
 PerangkinganModels.belongsTo(DataUsers, { as: 'daftarulang_oleh', foreignKey: 'daftar_ulang_by', targetKey: 'id' });
 PerangkinganModels.belongsTo(EzWilayahVerDapodiks, { as: 'data_wilayah_kec', foreignKey: 'kode_kecamatan', targetKey: 'kode_wilayah' });
