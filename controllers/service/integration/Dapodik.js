5322e28942665405daf697b6a8417a084bb80f7f
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-07-08 03:32:06 +0700

    dapodik

--- controllers/service/integration/Dapodik.js
+++ controllers/service/integration/Dapodik.js
@@ -1,14 +1,11 @@
 import axios from 'axios';
-import jwt from 'jsonwebtoken'; // sudah kamu sebutkan terpakai
 import EzAppKey from '../../../models/config/AppKeyModel.js';
 
 const API_URL = 'http://118.98.237.214'; // ganti dengan URL asli
-const USERNAME = 'masthenol@gmail.com';      // ganti dengan username asli
-const PASSWORD = 'Set@n2000$';      // ganti dengan password asli
-const TOKEN = '0CFA4030-9EBD-448B-A860-54EE711EA3A3';
+const USERNAME = 'masthenol@gmail.com';  // ganti dengan username asli
+const PASSWORD = 'Set@n2000$';          // ganti dengan password asli
 
 export const callAuthenticateV2 = async (req, res) => {
-    
   const url = `${API_URL}/v1/api-gateway/authenticate/authenticateV2/`;
 
   try {
@@ -36,34 +33,31 @@ export const callAuthenticateV2 = async (req, res) => {
           kode_random: `Bearer ${token}`,
         });
       } else {
-            await EzAppKey.create({
-            nama: 'dapodik',
-            apiKey: token,
-            kode_random: `Bearer ${token}`,
-            });
-        }
-        return res.status(200).json({
-            status: 1,
-            message: 'Token saved successfully'
-            token: token
-            // text: keyNya.kode_random
+        await EzAppKey.create({
+          nama: 'dapodik',
+          apiKey: token,
+          kode_random: `Bearer ${token}`,
         });
+      }
+      
+      return res.status(200).json({
+        status: 1,
+        message: 'Token saved successfully',
+        token: token
+      });
 
     } else {
-    
-            return res.status(200).json({
-                status:0,
-                 message: result?.message || 'Unauthorized'
-            });
-
+      return res.status(200).json({
+        status: 0,
+        message: result?.message || 'Unauthorized'
+      });
     }
   } catch (error) {
     const errMsg = error.response?.data?.message || error.message;
     
     return res.status(200).json({
-                status:0,
-                 message: errMsg
+      status: 0,
+      message: errMsg
     });
-
   }
 };
