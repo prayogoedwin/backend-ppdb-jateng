20834aeffc0760b0e41cff318a3a2ffd95cad42e
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-07-10 03:09:08 +0700

    0

--- controllers/service/integration/Dapodik.js
+++ controllers/service/integration/Dapodik.js
@@ -293,7 +293,7 @@ export const downloadCsvDonk = async (req, res) => {
       INNER JOIN ez_pendaftar c ON b.nik = c.nik
       INNER JOIN ez_sekolah_tujuan d ON a.sekolah_tujuan_id = d.id
       WHERE a.is_delete = 0
-        AND a.is_daftar_ulang = 1
+        AND a.is_daftar_ulang = 1 LIMIT 5
     `, {
       type: db3.QueryTypes.SELECT
     });
