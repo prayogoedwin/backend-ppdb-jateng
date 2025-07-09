8983ee71dea22068efd3cd7b6486c0532b3e0f1e
Edwin <you@example.comprayogo.edwin@gmail.com>
2025-07-09 10:24:51 +0700

    dapodik

--- controllers/service/integration/Dapodik.js
+++ controllers/service/integration/Dapodik.js
@@ -9,64 +9,60 @@ const PASSWORD = 'Set@n2000$';          // ganti dengan password asli
 export const callAuthenticateV2 = async (req, res) => {
   const url = `${API_URL}/v1/api-gateway/authenticate/authenticateV2/`;
 
-  const redis_key = `dapodik`; 
-
-//   let keyNya = await redisGet(redis_key);
-
+  const redis_key = `dapodik`;
   try {
+        const response = await axios.get(url, {
+        auth: {
+            username: USERNAME,
+            password: PASSWORD,
+        },
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        });
 
-                const response = await axios.get(url, {
-                auth: {
-                    username: USERNAME,
-                    password: PASSWORD,
-                },
-                headers: {
-                    'Content-Type': 'application/json',
-                },
-                });
+        const result = response.data;
 
-                const result = response.data;
+        if (result?.statusCode === 200 && result?.data?.token) {
+        const token = result.data.token;
 
-                if (result?.statusCode === 200 && result?.data?.token) {
-                const token = result.data.token;
+        // Update ke EzAppKey
+        const key = await EzAppKey.findOne({ where: { nama: 'dapodik' } });
 
-                // Update ke EzAppKey
-                const key = await EzAppKey.findOne({ where: { nama: 'dapodik' } });
+        if (key) {
+            await key.update({
+            apiKey: token,
+            kode_random: `Bearer ${token}`,
+            });
+        } else {
+            await EzAppKey.create({
+            nama: 'dapodik',
+            apiKey: token,
+            kode_random: `Bearer ${token}`,
+            });
+        }
 
-                if (key) {
-                    await key.update({
-                    apiKey: token,
-                    kode_random: `Bearer ${token}`,
-                    });
-                } else {
-                    await EzAppKey.create({
-                    nama: 'dapodik',
-                    apiKey: token,
-                    kode_random: `Bearer ${token}`,
-                    });
-                }
+        await redisSet(
+                redis_key,
+                JSON.stringify(result),
+                process.env.REDIS_EXPIRE_TIME_HARIAN
+            );
+        
+        return res.status(200).json({
+            status: 1,
+            message: 'Token saved successfully',
+            token: token
+        });
 
-                await redisSet(
-                        redis_key,
-                        JSON.stringify(keyNya),
-                        process.env.REDIS_EXPIRE_TIME_HARIAN
-                    );
-                
-                return res.status(200).json({
-                    status: 1,
-                    message: 'Token saved successfully',
-                    token: token
-                });
+        } else {
 
-                } else {
+            return res.status(200).json({
+                status: 0,
+                message: result?.message || 'Unauthorized'
+            });
 
-                    return res.status(200).json({
-                        status: 0,
-                        message: result?.message || 'Unauthorized'
-                    });
+        }
 
-                }
-                
 } catch (error) {
     const errMsg = error.response?.data?.message || error.message;
     
